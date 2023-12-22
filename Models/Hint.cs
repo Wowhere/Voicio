0487dc527ec3938bcad38585062fb778eebca6e8
John Doe <john@sample.com>
2023-12-22 23:36:38 +0300

    EF context added

--- Models/Hint.cs
+++ Models/Hint.cs
@@ -14,22 +14,22 @@ namespace voicio.Models
         
     }
 
-    public class Hint
-    {
-        public int Id { get; set; }
+    //public class Hint
+    //{
+    //    public int Id { get; set; }
 
-        public string Text { get; set; }
+    //    public string Text { get; set; }
 
-        public string? Comment { get; set; }
+    //    public string? Comment { get; set; }
 
-        public List<string>? Tags { get; set; }
+    //    public List<string>? Tags { get; set; }
 
-        public Hint(int id, string text, string comment)
-        {
-            Id = id; Text = text; Comment = comment;
-        }
-        public Hint(int id, string text, string comment, List<string> tags) {
-            Id = id; Text = text; Comment = comment; Tags = tags;
-        }
-    }
+    //    public Hint(int id, string text, string comment)
+    //    {
+    //        Id = id; Text = text; Comment = comment;
+    //    }
+    //    public Hint(int id, string text, string comment, List<string> tags) {
+    //        Id = id; Text = text; Comment = comment; Tags = tags;
+    //    }
+    //}
 }
--- Models/SqliteDataSource.cs
+++ Models/SqliteDataSource.cs
@@ -9,26 +9,54 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Data.Entity.Hierarchy;
+using Avalonia.Controls;
 
 namespace voicio.Models
 {
     public class Tag
     {
         public int Id { get; set; }
+        public string TagText { get; set; }
+        public List<HintTag> HintTag { get; } = new();
     }
-    public class Hints
+    public class Hint
     {
         public int Id { get; set; }
+        public string HintText { get; set; }
+        public int Comment { get; set; }
+        public List<HintTag> HintTag { get; } = new();
     }
-    public class SqliteDataSource : DbContext
+    public class HintTag
+    {
+        public int TagId { get; set; }
+        public int HintId { get; set; }
+        public Tag Tag { get; set; } = null!;
+        public Hint Hint { get; set; } = null!;
+    }
+    public class HelpContext : DbContext
     {
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Hint> Hints { get; set; }
         public string DbPath { get; }
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Hint>()
+                .Property(b => b.HintTag)
+                .HasDefaultValue(null);
+            modelBuilder.Entity<Tag>()
+                .Property(b => b.HintTag)
+                .HasDefaultValue(null);
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+
+        HelpContext()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            DbPath = System.IO.Path.Join(path, "helper.db");
         }
     }
 
