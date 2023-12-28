25cd8207640f0d96c31a5e1f9432d2c0469fb228
John Doe <john@sample.com>
2023-12-28 23:52:47 +0300

    Then remake to TreeDataGrid

--- Models/Hint.cs
+++ Models/Hint.cs
@@ -13,23 +13,4 @@ namespace voicio.Models
         public string Query { get; set; }
         
     }
-
-    //public class Hint
-    //{
-    //    public int Id { get; set; }
-
-    //    public string Text { get; set; }
-
-    //    public string? Comment { get; set; }
-
-    //    public List<string>? Tags { get; set; }
-
-    //    public Hint(int id, string text, string comment)
-    //    {
-    //        Id = id; Text = text; Comment = comment;
-    //    }
-    //    public Hint(int id, string text, string comment, List<string> tags) {
-    //        Id = id; Text = text; Comment = comment; Tags = tags;
-    //    }
-    //}
 }
